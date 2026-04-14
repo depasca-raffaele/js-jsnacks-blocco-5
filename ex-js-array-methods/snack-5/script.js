9a1c21dba4557a067b903d6db6a04c488f04fccd
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-04-14 18:52:09 +0200

    commit snack5

--- ex-js-array-methods/snack-5/script.js
+++ ex-js-array-methods/snack-5/script.js
@@ -2,3 +2,10 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+let evenNums = nums.filter(function(number){
+    return number % 2 === 0;
+});
+evenNums.forEach(function(element){
+  console.log(element);
+});
